8f6530e3bb2d80e18c4e8a332fa785319928ea82
Rishit Desai <rishit-desai@users.noreply.github.com>
2021-08-24 16:00:01 +0530

    kuch toh kiya

--- pages/Bio.js
+++ pages/Bio.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import NavCustom from '../components/NavCustom';
-
+import Arts from '../components/Arts';
 
 export default function Bio({ userId, currentUser })
 {
     return (
         <>
             <NavCustom />
+            <div style={{ background: "black" }}>
+                <Arts />
+            </div>
         </>
     )
 }
