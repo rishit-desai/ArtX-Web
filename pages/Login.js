91c525c51f2eb91773330bcb5ae05974a3f00d18
parnavh <45985534+parnavh@users.noreply.github.com>
2021-09-02 11:09:38 +0530

    firebase, login added

--- pages/Login.js
+++ pages/Login.js
@@ -1,13 +1,23 @@
 import Link from 'next/link';
-import React from 'react'
+import React, { useRef } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import NavCustom from '../components/NavCustom'
+import { auth } from '../lib/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+function login(email, password) {
+    signInWithEmailAndPassword(auth, email, password).then( user => console.log)
+}
+
 
 export default function Login()
 {
+    const email = useRef()
+    const password = useRef()
     function handleSubmit(e)
     {
         e.preventDefault();
+        login(email.current.value, password.current.value)
     }
     return (
         <>
@@ -18,13 +28,13 @@ export default function Login()
                     <h1>Artx</h1>
                     <Form onSubmit={handleSubmit}>
                         <Form.Group className="group">
-                            <Form.Control className="input" type="text" required />
+                            <Form.Control className="input" type="text" required ref={email}/>
                             <span className="highlight"></span>
                             <span className="bar"></span>
                             <Form.Label>E-mail</Form.Label>
                         </Form.Group>
                         <Form.Group className="group">
-                            <Form.Control className="input" type="password" required />
+                            <Form.Control className="input" type="password" required ref={password}/>
                             <span className="highlight"></span>
                             <span className="bar"></span>
                             <Form.Label>Password</Form.Label>
